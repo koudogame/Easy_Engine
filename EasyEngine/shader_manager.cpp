1231138f6d1f1b11d8eb971f0486421e07bf8259
Abya <harutch.222@gmail.com>
2020-01-14 20:02:59 +0900

    シェーダーマネージャ―の作成終了

--- EasyEngine/shader_manager.cpp
+++ EasyEngine/shader_manager.cpp
@@ -38,5 +38,115 @@ ShaderManager::~ShaderManager()
 
     p_loader_->release();
 }
+
+// 頂点シェーダの読み込み
+bool ShaderManager::loadVertexShader( const char* Path, IVertexShader** ppShader )
+{
+    // 読み込み
+    if( p_loader_->loadVertexShader(Path, ppShader) == false )
+        return false;
+
+    // リストに保持する
+    vs_list_.emplace( *ppShader );
+
+    return true;
+}
+// 頂点シェーダの解放
+void ShaderManager::releaseVertexShader( IVertexShader** ppShader )
+{
+    // リストに存在するシェーダーのみ解放の対象とする
+    auto find = vs_list_.find( *ppShader );
+    if( find != vs_list_.end() )
+    {
+        // この解放で実際に開放される場合、リストから削除
+        if( (*ppShader)->getRefCount() == 1U )
+        {
+            vs_list_.erase( find );
+        }
+
+        // 解放
+        (*ppShader)->release();
+    }
+}
+
+// ジオメトリシェーダ―の読み込み
+bool ShaderManager::loadGeometryShader( const char* Path, IGeometryShader** ppShader )
+{
+    // 読み込み
+    if( p_loader_->loadGeometryShader(Path, ppShader) == false )
+        return false;
+
+    // リストに保持する
+    gs_list_.emplace( *ppShader );
+
+    return true;
+}
+// ジオメトリシェーダーの解放
+void ShaderManager::releaseGeometryShader( IGeometryShader** ppShader )
+{
+    // リストに存在するシェーダーのみ対象とする
+    auto find = gs_list_.find( *ppShader );
+    if( find != gs_list_.end() )
+    {
+        // この解放で実際に開放される場合、リストから削除
+        if( (*ppShader)->getRefCount() == 1U )
+        {
+            gs_list_.erase( find );
+        }
+
+        // 解放
+        (*ppShader)->release();
+    }
+}
+
+// ピクセルシェーダ―の読み込み
+bool ShaderManager::loadPixelShader( const char* Path, IPixelShader** ppShader )
+{
+    // 読み込み
+    if( p_loader_->loadPixelShader(Path, ppShader) == false )
+        return false;
+
+    // リストに保持する
+    ps_list_.emplace( ppShader );
+
+    return true;
+}
+// ピクセルシェーダ―の解放
+void ShaderManager::releasePixelShader( IPixelShader** ppShader )
+{
+    // リストの存在するシェーダーのみ対象とする
+    auto find = ps_list_.find( *ppShader );
+    if( find != ps_list_.end() )
+    {
+        // 個の解放で実際に開放される場合、リストから削除
+        if( (*ppShader)->getRefCount() == 1U )
+        {
+            ps_list_.erase( find );
+        }
+
+        // 解放
+        (*ppShader)->release();
+    }
+}
+
+// 生成処理
+void ShaderManager::create( EasyEngine* pCreator, IShaderLoader* pLoader )
+{
+    pCreator->proof();
+
+    destroy( pCreator );
+    instance_ = new ShaderManager( pLoader );
+}
+// 破棄処理
+void ShaderManager::destroy( EasyEngine* pDeleter )
+{
+    pDeleter->proof();
+
+    if( instance_ )
+    {
+        delete instance_;
+        instance_ = nullptr;
+    }
+}
 END_EGEG
 // EOF
