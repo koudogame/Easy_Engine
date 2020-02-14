304b34f835129f1649772194034702b93e0addda
板場 <harutch.222@gmail.com>
2020-02-14 17:37:59 +0900

    UIDの仕様を変更した。

--- EasyEngine_verGameAwards/uid.hpp
+++ EasyEngine_verGameAwards/uid.hpp
@@ -9,29 +9,15 @@
 /// @class  UID
 /// @brief  型情報からIDを取得するクラス
 /// 
+template <class Object>
 class UID
 {
 public :
-    ///
-    /// @brief  型からIDを取得する
-    ///
-    /// @tparam Object : IDを取得するクラス型
-    ///
-    /// @return ID
-    ///
-    template <class Object>
-    static uint32_t getID() noexcept { static uint32_t uid = reinterpret_cast<uint32_t>(&uid); return uid; }
-
-    ///
-    /// @brief  インスタンスからIDを取得する
-    ///
-    /// @tparam Object : IDを取得するクラス型(省略可)
-    /// @param[in] : IDを取得するオブジェクトインスタンス
-    ///
-    /// @return ID
-    ///
-    template <class Object>
-    static uint32_t getID( const Object& ) noexcept { return UID::getID<Object>(); }
+    operator uint32_t() noexcept
+    {
+        static uint32_t uid = reinterpret_cast<uint32_t>(&uid);
+        return uid;
+    }
 };
 #endif /// !INCLUDED_EGEG_UID_HEADER_
 /// EOF
