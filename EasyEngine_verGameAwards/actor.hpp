46fbeec38435f335fa90fbc825c00392fc4c8d83
Abya <harutch.222@gmail.com>
2020-02-16 23:14:24 +0900

    今日の作業終了

--- EasyEngine_verGameAwards/actor.hpp
+++ EasyEngine_verGameAwards/actor.hpp
@@ -4,6 +4,7 @@
 ///
 #ifndef INCLUDED_ACTOR_HEADER_
 #define INCLUDED_ACTOR_HEADER_
+#include <cstdint>
 #include "game_unit.hpp"
 BEGIN_EGEG
 ///
@@ -14,9 +15,12 @@ class Actor :
     public GameUnit
 {
 public :
+    Actor( uint32_t ActorID ) noexcept : id_( ActorID ) {}
+
+    uint32_t getID() const noexcept { return id_; }
 
 private :
-
+    uint32_t id_;   /// アクター識別ID
 };
 END_EGEG
 #endif /// INCLUDED_ACTOR_HEADER_
--- EasyEngine_verGameAwards/game_unit.hpp
+++ EasyEngine_verGameAwards/game_unit.hpp
@@ -4,6 +4,7 @@
 ///
 #ifndef INCLUDED_EGEG_GAME_UNIT_HEADER_
 #define INCLUDED_EGEG_GAME_UNIT_HEADER_
+#include <cstdint>
 #include "egeg.hpp"
 BEGIN_EGEG
 ///
@@ -14,6 +15,23 @@ class GameUnit
 {
 public :
     virtual ~GameUnit() = default;
+
+    ///
+    /// @brief  初期化処理
+    ///
+    /// @return 成功[ true ]　失敗[ false ]
+    ///
+    virtual bool initialize() = 0;
+    ///
+    /// @brief  終了処理
+    ///
+    virtual void finalize() = 0;
+    ///
+    /// @brief  更新処理
+    ///
+    /// @param[in] DeltaTimeMS : 前回呼び出し時からの時差(ミリ秒)
+    ///
+    virtual void update( uint64_t DeltaTimeMS ) = 0;
 };
 END_EGEG
 #endif /// INCLUDED_EGEG_GAME_UNIT_HEADER_
