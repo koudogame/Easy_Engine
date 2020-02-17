d9ffbc3b43827346c3b78969b819afe9f7e080ec
板場 <harutch.222@gmail.com>
2020-02-17 16:46:01 +0900

    学校での作業完了

---
+++ EasyEngine_verGameAwards/actor2d.hpp
@@ -0,0 +1,23 @@
+///
+/// @file   actor2d.hpp
+/// @author 板場
+///
+#ifndef INCLUDED_EGEG_ACTOR2D_HEADER_
+#define INCLUDED_EGEG_ACTOR2D_HEADER_
+#include "actor.hpp"
+BEGIN_EGEG
+///
+/// @class  Actor2D
+/// @brief  2Dアクター
+///
+class Actor2D :
+    public Actor
+{
+public :
+
+private :
+
+};
+END_EGEG
+#endif /// INCLUDED_EGEG_ACTOR2D_HEADER_
+/// EOF
